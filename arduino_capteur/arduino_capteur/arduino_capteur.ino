48d877d802d2bffd297d70e784f2ffc790a6acb2
ayb2007 <ayoub.oumouma@gmail.com>
2026-05-25 15:07:55 +0200

    Ajout du code pour la carte Arduino UNO R4 WiFi

---
+++ arduino_capteur/arduino_capteur/arduino_capteur.ino
@@ -0,0 +1,41 @@
+#include <WiFiS3.h>
+
+char ssid[] = "TON_RESEAU_WIFI";
+char pass[] = "TON_MOT_DE_PASSE";
+
+char server[] = "192.168.X.X";
+int port = 8001;
+
+int status = WL_IDLE_STATUS;
+WiFiClient client;
+
+const int pinCapteur = 4;
+int etatPrecedent = LOW;
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(pinCapteur, INPUT_PULLUP);
+
+  while (status != WL_CONNECTED) {
+    status = WiFi.begin(ssid, pass);
+    delay(5000);
+  }
+}
+
+void loop() {
+  int etatActuel = digitalRead(pinCapteur);
+
+  if (etatActuel != etatPrecedent) {
+    if (client.connect(server, port)) {
+      client.println("POST /api/spots/A1/toggle HTTP/1.1");
+      client.print("Host: ");
+      client.println(server);
+      client.println("Content-Type: application/json");
+      client.println("Content-Length: 0");
+      client.println();
+      client.stop();
+    }
+    etatPrecedent = etatActuel;
+    delay(1000);
+  }
+}
